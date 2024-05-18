b66223e7c46e92710d37e1c2281c175077f83cf8
pranav2012 <p2012agarwal@gmail.com>
2024-05-19 03:28:02 +0530

    chore: prettier files changes

--- src/core/__tests__/sdk.test.ts
+++ src/core/__tests__/sdk.test.ts
@@ -1,35 +1,35 @@
-import { Config } from "..";
+import { Config } from '..';
 
 describe('sdk', () => {
-    let config: Config;
+	let config: Config;
 
-    beforeEach(() => {
-        config = {
-            baseURL: 'https://api.example.com',
-            timeout: 1000,
-            analyticsEndpoint: 'https://analytics.example.com',
-            sdkKey: '1234',
-            wsEndpoint: 'wss://ws.example.com',
-            onPreventionStatus: () => { },
-            apiCacheTTL: 60,
-            headers: { 'X-Api-Key': '1234' },
-            enableCache: true
-        };
-    });
+	beforeEach(() => {
+		config = {
+			baseURL: 'https://api.example.com',
+			timeout: 1000,
+			analyticsEndpoint: 'https://analytics.example.com',
+			sdkKey: '1234',
+			wsEndpoint: 'wss://ws.example.com',
+			onPreventionStatus: () => {},
+			apiCacheTTL: 60,
+			headers: { 'X-Api-Key': '1234' },
+			enableCache: true,
+		};
+	});
 
-    it('should have a config object', () => {
-        expect(config).toBeDefined();
-    });
+	it('should have a config object', () => {
+		expect(config).toBeDefined();
+	});
 
-    it('should have the correct properties in the config object', () => {
-        expect(config.baseURL).toBeDefined();
-        expect(config.timeout).toBeDefined();
-        expect(config.analyticsEndpoint).toBeDefined();
-        expect(config.sdkKey).toBeDefined();
-        expect(config.wsEndpoint).toBeDefined();
-        expect(config.onPreventionStatus).toBeDefined();
-        expect(config.apiCacheTTL).toBeDefined();
-        expect(config.headers).toBeDefined();
-        expect(config.enableCache).toBeDefined();
-    });
+	it('should have the correct properties in the config object', () => {
+		expect(config.baseURL).toBeDefined();
+		expect(config.timeout).toBeDefined();
+		expect(config.analyticsEndpoint).toBeDefined();
+		expect(config.sdkKey).toBeDefined();
+		expect(config.wsEndpoint).toBeDefined();
+		expect(config.onPreventionStatus).toBeDefined();
+		expect(config.apiCacheTTL).toBeDefined();
+		expect(config.headers).toBeDefined();
+		expect(config.enableCache).toBeDefined();
+	});
 });
--- src/core/sdk.ts
+++ src/core/sdk.ts
@@ -1,4 +1,4 @@
-import { Config } from ".";
+import { Config } from '.';
 
 let config: Config;
 
